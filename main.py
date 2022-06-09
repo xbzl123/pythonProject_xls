b4d6504c317a8db4c75542513f45ecdc9f140a26
longyanghe <742781499@qq.com>
2022-06-09 19:30:03 +0800

    feat:增加进度条和翻译功能

--- main.py
+++ main.py
@@ -2,34 +2,48 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import _thread
+import json
 import sys
-import tkinter
+
+import requests
 
 import xlrd as xlrd
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QFileDialog, QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QTableWidget, QTableWidgetItem, QProgressDialog, QMessageBox
+from xlrd.sheet import Sheet
 
 import filedialog
 import xls_dealwith
 
 app = QtWidgets.QApplication(sys.argv)
-tableWidget : QTableWidget
+tableWidget: QTableWidget
+table: Sheet
+list = []
+
 
 def readXls(strpath=""):
     workbook = xlrd.open_workbook_xls(strpath[0][0])
+    global table
     table = workbook.sheets()[0]
-    tableWidget.setItem(3,1,QTableWidgetItem('test'))
     # table0 = workbook.sheet_by_index(0)
     # 按行读取
+    tableWidget.setColumnCount(table.ncols)
+    tableWidget.setRowCount(table.nrows)
     for i in range(table.nrows):
+        print(table.row_values(i))
         for j in range(table.ncols):
-            tableWidget.setItem(i, j, QTableWidgetItem(table.cell(i, j).value))
+            value = table.cell(i, j).value
+            if isinstance(value, float):
+                value = str(int(value))
+            tableWidget.setItem(i, j, QTableWidgetItem(value))
         # tableWidget.viewport().update()
     # 按列读取
     # for i in range(table.ncols):
     #     print(table.col_values(i))
     # print(table.cell(3, 2).value)
-    pass
+    return table
 
 
 class parentWindow(QMainWindow):
@@ -42,8 +56,40 @@ class parentWindow(QMainWindow):
 def openfile():
         openfile_name =QFileDialog.getOpenFileNames(None, "请选择要添加的文件", "./", "Text Files (*.xls);;All Files (*)")
         print(openfile_name)
-        readXls(openfile_name)
+        return readXls(openfile_name)
 
+def networkrequest(tarlan = "en", content = "", colpos = 0):
+    # 网站地址
+    url = 'https://translate.google.cn/translate_a/single?client=gtx&sl=auto&tl=' + tarlan + '&dt=t&q=' + content
+    header = {'Connection': 'close'}
+
+    # 获取网页
+    r = requests.request('GET', url)
+    # requests.adapters.DEFAULT_RETRIES = 5
+    # r.encoding = r.apparent_encoding
+    load = json.loads(r.text)
+    result = load[0][0][0]
+    # print("the %s translat result is %s:", content, result)
+    # tableWidget.setItem(colpos, 1, QTableWidgetItem(result))
+    list.pop(0)
+    print(list.count())
+    # dialog.refreshProgress(list.count())
+    return result
+
+
+def translate():
+    global list,dialog
+    for i in range(table.nrows):
+        inputContent = table.cell(i, 1).value
+        if len(inputContent) > 0:
+            list.append(1)
+            # print("inputContent = "+inputContent)
+            try:
+                _thread.start_new_thread(networkrequest, ("en", inputContent, i))
+            except requests.exceptions.ConnectionError:
+                print("Error: unable to start thread")
+
+    dialog = myProgressDialog()
 
 
 class openFileDialog(QFileDialog):
@@ -52,7 +98,26 @@ class openFileDialog(QFileDialog):
         self.child = filedialog.Ui_Dialog()
         self.child.setupUi(self)
 
+class myProgressDialog:
+    global progress
+    def __init__(self):
+        progress = QProgressDialog(QProgressDialog())
+        progress.setWindowTitle("请稍等")
+        progress.setLabelText("正在操作...")
+        progress.setMinimumDuration(1)
+        progress.setWindowModality(Qt.WindowModal)
+        progress.setRange(0,list.count())
 
+    def refreshProgress(pro):
+        if (pro < list.count()):
+            progress.setValue(pro)
+            if progress.wasCanceled():
+                QMessageBox.warning(QProgressDialog(),"提示","操作失败")
+        else:
+            progress.setValue(list.count())
+            QMessageBox.information(QProgressDialog(),"提示","操作成功")
+
+dialog: myProgressDialog
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -92,6 +157,7 @@ if __name__ == '__main__':
 
     tableWidget = mainView.main_ui.tableWidget
     mainView.main_ui.openFile.clicked.connect(openfile)
+    mainView.main_ui.translate.clicked.connect(translate)
 
     mainView.show()
     sys.exit(app.exec_())
