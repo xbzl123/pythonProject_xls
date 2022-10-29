ff5e95e52f2a36f2d4538d4b6e23d619aa866e66
longyanghe <742781499@qq.com>
2022-10-29 20:29:40 +0800

    feat:增加代理IP检测

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ from PyQt5.QtWidgets import QFileDialog, QMainWindow, QTableWidget, QTableWidget
 
 import filedialog
 import xls_dealwith
+
 app = QtWidgets.QApplication(sys.argv)
 tableWidget: QTableWidget
 targetlanguage = "en"
 workbook = None
+
 
 def readXls(strpath=""):
     print(strpath)
@@ -44,6 +46,7 @@ def readXls(strpath=""):
             tableWidget.setItem(i, j, QTableWidgetItem(value))
     return table
 
+
 def writeXls(strpath=""):
     global workbook
     savebook = xlwt.Workbook('utf-8')
@@ -51,7 +54,7 @@ def writeXls(strpath=""):
     oldsheet = workbook.sheets()[0]
     for i in range(oldsheet.nrows):
         for j in range(oldsheet.ncols):
-            sheet.write(i, j,oldsheet.cell(i, j).value)
+            sheet.write(i, j, oldsheet.cell(i, j).value)
             print(oldsheet.cell(i, j).value)
     print(strpath)
     savebook.save(strpath[0])
@@ -65,17 +68,20 @@ class parentWindow(QMainWindow):
         self.setWindowTitle("xls 处理")
 
 
-proxie = ""
 def openfile():
-    getProxyAddress()
     openfile_name = QFileDialog.getOpenFileNames(None, "请选择要添加的文件", "./", "Text Files (*.xls);;All Files (*)")
     print(openfile_name)
     return readXls(openfile_name)
 
+
 def savefile():
+    if workbook == None:
+        QMessageBox.about(QDialog(), "Notice", "please open a xls file frist!")
+        return
     savefile_name = QFileDialog.getSaveFileName(None, "保存文件", "./", "Text Files (*.xls);;All Files (*)")
     print(savefile_name)
     writeXls(savefile_name)
+
 
 class MyThread(threading.Thread):
     #  通过类成员对象定义信号对象
@@ -83,62 +89,124 @@ class MyThread(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
+
     def run(self):
         for i in range(100):
             self._signal.emit(i)  # 发送实时任务进度和总任务进度
             time.sleep(0.1)
 
 
-def getProxyAddress():
+activedproxies = []
+checkactivedipnum = 0
+
+def getproxyaddress():
+    try:
         # 网站地址
         url = 'https://proxy.seofangfa.com/'
-        head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
+        head = {  # 模拟浏览器头部信息，向服务器发送消息
             "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
         }
         r = requests.get(url, headers=head)
         dom = etree.HTML(r.text)
         url_path = '//td'
         urls = dom.xpath(url_path)
-        global proxie
-        proxie = 'http://'+ urls[0].text + ':' + urls[1].text
-        print("the len(selectedlist) is %s:", proxie)
+        proxies = []
+        for i in range(10):
+            print("i is %d:", i)
+            proxie = 'http://' + urls[i * 5].text + ':' + urls[i * 5 + 1].text
+            proxies.append(proxie)
+        print("the len(selectedlist) is %s:", proxies)
+    except requests.exceptions.ConnectionError:
+        print("Error: unable to connect success")
+    finally:
+        urls = ["http://httpbin.org/ip", "https://www.baidu.com", "https://www.google.com.hk", "https://www.sohu.com",
+                "https://mail.qq.com", "https://www.sina.com.cn", "https://mail.10086.cn", "https://www.sogou.com",
+                "https://juejin.cn", "https://mail.sina.com.cn"]
+        print('开始IP检测')
+        global checkactivedipnum
+        checkactivedipnum = 0
+        global activedproxies
+        activedproxies = []
+        try:
+            for i in range(len(proxies)):
+                _thread.start_new_thread(checkactivedip, (proxies[i], urls[i], len(proxies)))
+        except Exception as e:
+            print('start thread :' + e.args[0])
+        finally:
+            while True:
+                if checkactivedipnum == len(proxies):
+                    starttranslate(activedproxies)
+                    break
+                    print('结束IP检测'+str(len(proxies)))
+
+
+# 筛选有效的代理IP
+def checkactivedip(proxy="", url="", num=0):
+    try:
+        head = {  # 模拟浏览器头部信息，向服务器发送消息
+            "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
+        }
+        proxieswrap = {'http': proxy}
+        response = requests.get(url, headers=head, proxies=proxieswrap, timeout=0.1)
+        print(response.status_code)
+        print('当前IP有效:' + proxy)
+        global activedproxies
+        activedproxies.append(proxy)
+        # starttranslate(proxies[i])
+    except Exception as e:
+        print('当前IP无效:' + proxy)
+    finally:
+        global checkactivedipnum
+        checkactivedipnum = checkactivedipnum+1
+        if checkactivedipnum == num:
+            print('代理IP的长度:' + str(len(activedproxies)))
 
 def pbar_change(progress):
     global so
     so.progress_update.emit(progress)
 
+#并发任务进行翻译
 
-def networkrequest(tarlan="", translatelist=[], proxie =""):
+
+#串联任务进行翻译
+def networkrequest(tarlan="", translatelist=[], proxies=[]):
     for i in range(len(translatelist)):
         try:
             # 网站地址
-            url = 'https://translate.google.cn/translate_a/single?client=gtx&sl=auto&tl=' + tarlan + '&dt=t&q=' + translatelist[i].text()
+            url = 'https://translate.google.hk/translate_a/single?client=gtx&sl=auto&tl=' + tarlan + '&dt=t&q=' + \
+                  translatelist[i].text()
             head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
                 "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
             }
             # 用户代理，表示告诉豆瓣服务器，我们是什么类型的机器、浏览器（本质上是告诉浏览器，我们可以接收什么水平的文件内容）
-            proxies = {'http': proxie}
+            proxieswrap = {'http': proxies[0]}
             # 获取网页
-            r = requests.get(url, headers=head, proxies=proxies)
-
-            load = json.loads(r.text)
-            result = load[0][0][0]
-            print("the %s translat result is %s:", translatelist[i].text(), result)
-            tableWidget.setItem(translatelist[i].row(), translatelist[i].column(), QTableWidgetItem(result))
-            global translatenum,so
-            translatenum = translatenum + 1
-            progress = int(translatenum / len(translatelist) * 100)
-            print("the progress is %s:", progress)
-            # childView.setProgress(progress)
-            worker = threading.Thread(target=pbar_change(progress))
-            worker.start()
+            r = requests.get(url, headers=head, proxies=proxieswrap)
+            if r.status_code == 200:
+                load = json.loads(r.text)
+                result = load[0][0][0]
+                print("the %s translat result is %s:", translatelist[i].text(), result)
+                tableWidget.setItem(translatelist[i].row(), translatelist[i].column(), QTableWidgetItem(result))
+                global translatenum, so
+                translatenum = translatenum + 1
+                progress = int(translatenum / len(translatelist) * 100)
+                print("the progress is %s:", progress)
+                # childView.setProgress(progress)
+                worker = threading.Thread(target=pbar_change(progress))
+                worker.start()
+            else:
+                QMessageBox.about(QDialog(), "Notice", r.text)
         except requests.exceptions.ConnectionError:
             print("Error: unable to start thread")
         time.sleep(0.05)
 
-#翻译选中的单元格的内容为指定语言
-def translate():
 
+# 翻译选中的单元格的内容为指定语言
+def translate():
+    getproxyaddress()
+
+
+def starttranslate(proxies):
     global translatenum
     translatenum = 0
     global childView
@@ -149,8 +217,6 @@ def translate():
     elif len(tableWidget.selectedItems()) < 1:
         QMessageBox.about(QDialog(), "Notice", "please select data to translate!")
         return
-
-
 
     for i in range(len(tableWidget.selectedItems())):
         selectedlist.append(tableWidget.selectedItems()[i])
@@ -166,8 +232,9 @@ def translate():
     so = SignalStore()
     childView = ProgressBar()
 
-    # #多线程调用网络接口翻译
-    _thread.start_new_thread(networkrequest, (targetlanguage, translatelist, proxie))
+    # 多线程调用网络接口翻译
+    _thread.start_new_thread(networkrequest, (targetlanguage, translatelist, proxies))
+
 
 class openFileDialog(QFileDialog):
     def __init__(self):
@@ -175,7 +242,8 @@ class openFileDialog(QFileDialog):
         self.child = filedialog.Ui_Dialog()
         self.child.setupUi(self)
 
-def selecttargetlanguage(lan = ""):
+
+def selecttargetlanguage(lan=""):
     global targetlanguage
     targetlanguage = lan
     print('targetlanguage %s', targetlanguage)
@@ -221,6 +289,7 @@ class ProgressBar(QWidget):
             tableWidget.viewport().update()
             return
 
+
 def convertxls():
     if workbook is None:
         QMessageBox.about(QDialog(), "Notice", "please open a xls file frist!")
@@ -231,14 +300,18 @@ def convertxls():
         if mainView.main_ui.includeHeader.isChecked():
             for i in range(sheets_.nrows):
                 if sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value is not None:
-                    contentxls += '\t<string name="'+ str(sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value) + '">' \
-                                  + str(sheets_.cell(i, int(mainView.main_ui.valueEdit.text()) - 1).value)+'</string>\n'
+                    contentxls += '\t<string name="' + str(
+                        sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value) + '">' \
+                                  + str(
+                        sheets_.cell(i, int(mainView.main_ui.valueEdit.text()) - 1).value) + '</string>\n'
         else:
             for i in range(sheets_.nrows):
                 if sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value is not None and i > 0:
-                    contentxls += '\t<string name="'+ str(sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value) + '">' \
-                                  + str(sheets_.cell(i, int(mainView.main_ui.valueEdit.text()) - 1).value)+'</string>\n'
-        contentxls +='</resources>'
+                    contentxls += '\t<string name="' + str(
+                        sheets_.cell(i, int(mainView.main_ui.keyEdit.text()) - 1).value) + '">' \
+                                  + str(
+                        sheets_.cell(i, int(mainView.main_ui.valueEdit.text()) - 1).value) + '</string>\n'
+        contentxls += '</resources>'
         print(contentxls)
 
         savefile_name = QFileDialog.getSaveFileName(None, "保存文件", "./", "Text Files (*.xml);;All Files (*)")
@@ -265,7 +338,6 @@ if __name__ == '__main__':
     mainView.main_ui.toFrench.clicked.connect(lambda: selecttargetlanguage("fr"))
 
     mainView.main_ui.toXml.clicked.connect(convertxls)
-
 
     mainView.show()
     sys.exit(app.exec_())
